b58a42e3bd0166d72b07c3f5550483cc011fdf1c
Kunal Kashyap <kunal.kashyap@gmail.com>
2016-12-27 15:41:52 +0530

    First page changes

--- hello.py
+++ hello.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello():
     return render_template("index.html")
 
+@app.route("/create/")
+def create():
+	return render_template("secondpage.html")
+
 if __name__ == "__main__":
     app.run()
