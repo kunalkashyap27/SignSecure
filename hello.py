535c285fd1d850e2350f48d6e02b147ed96fe442
Kunal Kashyap <kunal.kashyap@gmail.com>
2017-03-06 11:19:32 +0530

    Updated pages

--- hello.py
+++ hello.py
@@ -13,6 +13,18 @@ def create():
 def review():
 	return render_template("review.html")
 
+@app.route("/doc_org/")
+def doc_org():
+	return render_template("doc_org.html")
+
+@app.route("/doc_editor/")
+def doc_editor():
+	return render_template("doc_editor.html")
+
+@app.route("/doc_signer/")
+def doc_signer():
+	return render_template("doc_signer.html")
+
 @app.route("/check_status/")
 def check_status():
 	return render_template("check_status.html")
